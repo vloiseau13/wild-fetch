16ecd14e905b7b3a0b0db8df0dbed1c6f28778b4
vloiseau13 <valentin.loiseau13@gmail.com>
2024-10-30 09:50:35 +0100

    Fetch

---
+++ src/App.tsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import EmployeeCard from "./components/EmployeeCard";
+
+const sampleEmployee = {
+  name: {
+    first: "Charlie",
+    last: "Thompson",
+  },
+  email: "charlie.thompson@example.com",
+  picture: {
+    medium: "https://randomuser.me/api/portraits/med/men/40.jpg",
+  },
+};
+
+function App() {
+  const [employee, setEmployee] = useState(sampleEmployee);
+  const getEmployee = () => {
+    // Send the request
+    fetch("https://randomuser.me/api?nat=en")
+      .then((response) => response.json())
+      .then((data) => {
+        setEmployee(data.results[0]);
+      });
+  };
+  return (
+    <div className="App">
+      <EmployeeCard employee={employee} />
+      <button type="button" onClick={getEmployee}>
+        Get employee
+      </button>
+    </div>
+  );
+}
+
+export default App;
